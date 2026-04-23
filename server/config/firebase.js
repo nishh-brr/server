7ec190062b94dfe33694ae77e410d5d81713a76e
navyasyal <navyaasyall@gmail.com>
2026-04-23 11:14:41 +0530

    updates

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -7,13 +7,20 @@
 
 // module.exports = admin;
 const admin = require('firebase-admin');
-const serviceAccount = require('../firebase_key.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+let serviceAccount;
+
+if (process.env.FIREBASE_KEY) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+} else {
+  serviceAccount = require('../firebase-key.json');
+}
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+}
 
 const db = admin.firestore();
-
 module.exports = db;
-
